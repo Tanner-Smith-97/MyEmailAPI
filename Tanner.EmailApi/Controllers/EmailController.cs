3dfa5e776bfaf25de093bd84c22b34bf6a3ed93d
Tanner S <smithjtanner@gmail.com>
2021-09-10 22:35:15 -0600

    Started a project to handle emails for my website.

---
+++ Tanner.EmailApi/Controllers/EmailController.cs
@@ -0,0 +1,11 @@
+﻿namespace Tanner.EmailApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController
+    {
+        
+    }
+}
